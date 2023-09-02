aedb736749dbbec18942cd532edaba7e94293c69
Ac-bin <1052969874@qq.com>
2023-09-02 17:40:10 +0800

    dev修改
    Signed-off-by: Ac-bin <1052969874@qq.com>

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -6,6 +6,7 @@ public class HelloGit {
         System.out.println("新疆");
         System.out.println("第一次修改");
         System.out.println("第二次修改");
+        System.out.println("dev修改");
     }
 
 }
