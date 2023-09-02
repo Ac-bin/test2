ec3951b38fed18aa555064ea71f5e644fb739f60
Ac-bin <1052969874@qq.com>
2023-09-02 16:42:43 +0800

    第二次更新

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("新疆");
         System.out.println("第一次修改");
+        System.out.println("第二次修改");
     }
 
 }
